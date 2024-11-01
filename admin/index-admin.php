d07a1af927b3d6b35d137d1ababe17f018dcde62
alan-desing <alanbissio@gmail.com>
2024-11-01 18:24:21 -0300

    holi

--- admin/index-admin.php
+++ admin/index-admin.php
@@ -437,6 +437,7 @@ section {
 
 
     <script>
+        
         let indiceImagen = 0; 
         const imagenes = document.querySelectorAll('.imagenes img');
         const totalImagenes = imagenes.length;
